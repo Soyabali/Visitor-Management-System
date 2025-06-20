86d887cec7c2082180be1b6094708443fd0ed723
Soyabali <soyabali64@gmail.com>
2025-06-20 10:56:46 +0530

    change a toast colore

--- lib/presentation/visitorEntry/visitorEntry.dart
+++ lib/presentation/visitorEntry/visitorEntry.dart
@@ -990,7 +990,7 @@ class _VisitorEntryScreenState extends State<VisitorEntryScreen> {
       toastLength: Toast.LENGTH_SHORT,
       gravity: ToastGravity.CENTER,
       timeInSecForIosWeb: 1,
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.green,
       textColor: Colors.white,
       fontSize: 16.0,
     );
